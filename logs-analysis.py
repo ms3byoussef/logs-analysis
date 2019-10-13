df9034b26a61e31a8463609897fd71b02240a0ec
مصعب يوسف <ms3byoussef@gmail.com>
2019-10-13 06:22:45 +0200

    Update logs-analysis.py

--- logs-analysis.py
+++ logs-analysis.py
@@ -1,62 +1,75 @@
-#!/usr/bin/env python3
-# logs-analysis  06-10-2019
+#! /usr/bin/env python3
+
+  
+def get_query_result(query):
+    db = psycopg2.connect(database=DB_NAME)
+    cursor = db.cursor()
+    cursor.execute(query)
+    results = cursor.fetchall()
+    db.close()
+    return results  
 
 
-import psycopg2
+# What are the most popular three articles of all time?
+query_1_title = ("What are the most popular three articles of all time?")
+query_1 = (
+    "select articles.title, count(*) as views "
+    "from articles inner join log on log.path "
+    "like concat('%', articles.slug, '%') "
+    "where log.status like '%200%' group by "
+    "articles.title, log.path order by views desc limit 3")
+
+# Who are the most popular article authors of all time?
+query_2_title = ("Who are the most popular article authors of all time?")
+query_2 = (
+    "select authors.name, count(*) as views from articles inner "
+    "join authors on articles.author = authors.id inner join log "
+    "on log.path like concat('%', articles.slug, '%') where "
+    "log.status like '%200%' group "
+    "by authors.name order by views desc")
+
+# On which days did more than 1% of requests lead to errors
+query_3_title = ("On which days did more than 1% of requests lead to errors?")
+query_3 = (
+    "select day, perc from ("
+    "select day, round((sum(requests)/(select count(*) from log where "
+    "substring(cast(log.time as text), 0, 11) = day) * 100), 2) as "
+    "perc from (select substring(cast(log.time as text), 0, 11) as day, "
+    "count(*) as requests from log where status like '%404%' group by day)"
+    "as log_percentage group by day order by perc desc) as final_query "
+    "where perc >= 1")
 
 
-def main():
-    # Connect to an existing database
-    connect = psycopg2.connect("dbname=news")
+def get_query_result(query):
+    db = psycopg2.connect(database=DB_NAME)
+    cursor = db.cursor()
+    cursor.execute(query)
+    results = cursor.fetchall()
+    db.close()
+    return results  
 
-    # Open a cursor to perform database operations
-    cursor = connect.cursor()
+
+def print_query_results(query_results):
+    print (query_results[1])
+    for index, results (query_results[0]):
+        print (
+            "\t", index+1, "-", results[0],
+            "\t - ", str(results[1]), "views")
 
 
-# 1. What are the most popular three articles of all time?
-sql_popular_articles = """
-      SELECT article_view.title, article_view.view
-      FROM article_view
-      ORDER BY article_view.view DESC
-      LIMIT 3;
-    """
-    cursor.execute(sql_popular_articles)
-    print("Most popular articles:")
-    for (title, view) in cursor.fetchall():
-        print("    {} - {} views".format(title, view))
-    print("-" * 70)
-    
-# 2. Who are the most popular article authors of all time? 
-sql_popular_authors = """
-    SELECT article_view.name, SUM(article_view.view) AS author_view
-    FROM article_view
-    GROUP BY article_view.name
-    ORDER BY author_view DESC;
-    """
-    cursor.execute(sql_popular_authors)
-    print("Most popular authors:")
-    for (name, view) in cursor.fetchall():
-        print("    {} - {} views".format(name, view))
-    print("-" * 70)
-    
+def print_error_results(query_results):
+    print (query_results[1])
+    for results in query_results[0]:
+        print ("\t", results[0], "-", str(results[1]) + "% errors")
 
-# 3. On which days did more than 1% of requests lead to errors?
- sql_more_than_one_percent_errors = """
-    SELECT *
-    FROM error_rate
-    WHERE error_rate.percentage > 1
-    ORDER BY error_rate.percentage DESC;
-    """
-    cursor.execute(sql_more_than_one_percent_errors)
-    print("Days with more than 1% errors:")
-    for (date, percentage) in cursor.fetchall():
-        print("    {} - {}% errors".format(date, percentage))
-    print("-" * 70)
 
-    # Close communication with the database
-    cursor.close()
-    connent.close()
+if __name__ == '__main__':
+    # store query results
+    popular_articles_results = get_query_results(query_1), query_1_title
+    popular_authors_results = get_query_results(query_2), query_2_title
+    load_error_days = get_query_results(query_3), query_3_title
 
-if __name__ == "__main__":
-    main()
-
+    # print query results
+    print_query_results(popular_articles_results)
+    print_query_results(popular_authors_results)
+    print_error_results(load_error_days)
