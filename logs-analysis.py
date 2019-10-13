2c43c2f4e0584704cbcf281399ad85e6b9543c88
مصعب يوسف <ms3byoussef@gmail.com>
2019-10-13 06:40:17 +0200

    Update logs-analysis.py

--- logs-analysis.py
+++ logs-analysis.py
@@ -15,33 +15,26 @@ def get_query_result(query):
 
 # What are the most popular three articles of all time?
 query_1_title = ("What are the most popular three articles of all time?")
-query_1 = (
-    "select articles.title, count(*) as views "
-    "from articles inner join log on log.path "
-    "like concat('%', articles.slug, '%') "
-    "where log.status like '%200%' group by "
-    "articles.title, log.path order by views desc limit 3")
+query_1 = "select articles.title , count(log.path) as views from articles" \
+    " left join log on log.path like '%' || articles.slug where log.status " \
+    "= '200 OK' group by articles.title order by views desc limit 3"
 
 # Who are the most popular article authors of all time?
 query_2_title = ("Who are the most popular article authors of all time?")
-query_2 = (
-    "select authors.name, count(*) as views from articles inner "
-    "join authors on articles.author = authors.id inner join log "
-    "on log.path like concat('%', articles.slug, '%') where "
-    "log.status like '%200%' group "
-    "by authors.name order by views desc")
+query_2 = "select authors.name , t1.views from authors, (select articles.author" \
+    " as author , count(log.path) as views from articles left join log on" \
+    " log.path like '%' || articles.slug where log.status = '200 OK' group " \
+    "by articles.author order by views desc) as t1 where t1.author = " \
+    "authors.id"
 
 # On which days did more than 1% of requests lead to errors
 query_3_title = ("On which days did more than 1% of requests lead to errors?")
-query_3 = (
-    "select day, perc from ("
-    "select day, round((sum(requests)/(select count(*) from log where "
-    "substring(cast(log.time as text), 0, 11) = day) * 100), 2) as "
-    "perc from (select substring(cast(log.time as text), 0, 11) as day, "
-    "count(*) as requests from log where status like '%404%' group by day)"
-    "as log_percentage group by day order by perc desc) as final_query "
-    "where perc >= 1")
-
+query_3 = "select t1.date, round((t2.errors::NUMERIC / t1.total) * 100 ,1) as " \
+    "result from (select date_trunc('day', time) as date, count(1) as" \
+    " total from log group by 1) as t1, (select date_trunc('day', time)" \
+    " as date, count(1) as errors from log where log.status != '200 OK'" \
+    " group by 1) as t2 where t1.date = t2.date and " \
+    "round((t2.errors::NUMERIC / t1.total) * 100 ,1) > 1"
 
 def print_query_results(query_results):
     print (query_results[1])
