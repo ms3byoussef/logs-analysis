495249efa43a7c50dfef9f8241e59a5860e88604
مصعب يوسف <ms3byoussef@gmail.com>
2019-11-18 20:16:29 +0200

    Update logs-analysis.py

--- logs-analysis.py
+++ logs-analysis.py
@@ -1,102 +1,105 @@
-#! /usr/bin/env python3
-
 import psycopg2
-DBNAME = "news"
 
-def connect(DBNAME="news"):
-    """Connect to the  database."""
-    db = psycopg2.connect('dbname=' + DBNAME)
-    cursor = db.cursor()
-   
-    return db, cursor
+#connect to the database
+dbname="news"
 
-def get_results(query):
-    """Return query results for given query """
-    db, cursor = connect()
-    cursor.execute(query)
-    return cursor.fetchall()
-    db.close()
-    
-result=get_results(query)
-# What are the most popular three articles of all time?
 
-title_1 = ("What are the most popular three articles of all time?")
-query_1 = """
-        SELECT articles.title, COUNT(*) AS views
-        FROM articles
-        JOIN log
-        ON log.path LIKE concat('/article/%', articles.slug)
-        GROUP ByY articles.title
-        ORDER BY views DESC
-        LIMIT 3;
-   """
+conn= psycopg2.connect(database=dbname)
 
- 
-# Who are the most popular article authors of all time?
-title_2 = ("Who are the most popular article authors of all time?")
-query_2 = """"
-SELECT authors.name,COUNT(article_view.views) AS views
-FROM article_view,authors
-where authors.id = article_view.author
-GROUP By authors.name
-ORDER BY views desc"""
+#the cursor
 
-def print("\n2. Who are the most popular article authors of all time?")
- cursor.execute(query_2)
-   result= cursor.fetchall()
-for i in result:
-    
-# On which days did more than 1% of requests lead to errors
-title_3 = ("On which days did more than 1% of requests lead to errors?")
-query_3 = """
-SELECT total.day,
-          ROUND(((errors.eerror_results*1.0) / total.results), 3) AS perc
-        FROM (
-          SELECT date_trunc('day', time) "day", count(*) AS error_results
-          FROM log
-          WHERE status LIKE '404%'
-          GROUP BY day
-        ) AS errors
-        JOIN (
-          SELECT date_trunc('day', time) "day", count(*) AS results
-          FROM log
-          GROUP BY day
-          ) AS total
-        ON total.day = errors.day
-        WHERE (ROUND(((errors.error_results*1.0) / total.results), 3) > 0.01)
-        ORDER BY perc DESC;
- """
+c = conn.cursor()
 
- def print_query_1(query_result):
-    
-print("\n1. What are the most popular three articles of all time?")
-num=1
-for i in result: 
-      print('(' + str(num) + ')' + i [0] + " with ' + str(i[1]) + " views")
-        num += 1
+#the query title
 
-def print_query_2(query_result):
-    
-print("\n2. Who are the most popular article authors of all time?")
-num=1
-for i in result: 
-      print('(' + str(num) + ')' + i [0] + " with ' + str(i[1]) + " views")
-        num += 1
+query_title1 ="the most popular three articles of all time:"
+
+#query of the most popular articles
+
+query1="""select articles.title, count(*) as Views 
+from articles,log 
+where log.path = concat('/article/', articles.slug)
+group by articles.title 
+order by Views  desc limit 3;"""
+
+#the cursor execute 
+
+c.execute(query1)
+
+#the results
+
+myresults1=c.fetchall()
+
+#print the query
+
+print("The QUERY 1"+"\n"+query_title1 + "\n")
+
+for result in myresults1:
+        print('\t' + str(result[0]) + " - " + str(result[1]) + " Views  " + "\n")
 
 
 
+#the secand query 
+
+#the query title
+query_title2 ="the most popular article authors of all time:"
+
+#query of the most popular article authors
+
+query2=""" SELECT authors.name, COUNT(*) As Views 
+    FROM authors
+    INNER JOIN articles
+      ON authors.id = articles.author
+    INNER JOIN log
+      ON log.path LIKE concat('/article/', articles.slug)
+    GROUP BY authors.name
+    ORDER BY Views  desc;
+    """
+
+#the cursor execute 
+
+c.execute(query2)
+
+#the results
+
+myresults2=c.fetchall()
+
+#print the query
+
+print("The QUERY 2"+"\n"+query_title2 + "\n")
+
+for result in myresults2:
+       print('\t' + str(result[0]) + " - " + str(result[1]) + " views "+ "\n")
 
 
-def print_error(query-error):
-    print (" \n3.On which days did more than 1% of requests lead to errors?")
-    for result in  :
-        print ('\t' + str(result[0]) + '  ' + str(result[1]) + ' %')
- # the query result
-articles_results = get_result(query_1)
-authors_results = get_result(query_2)
-error_days_results= get_result(query_3)
+#the third query 
 
-# print the output
-print_query_1(articles_results)
-print_query_2(authors_results)
-print_error(error_days_results)
+#the query title
+query_title3 ="On which days did more than 1% of requests lead to errors"
+
+#query of On which days did more than 1% of requests lead to errors
+
+query3=""" SELECT *
+            FROM (select date(time), ROUND (100.0 * sum (CASE log.status
+            WHEN '200 OK' THEN 0 else 1 end)
+            /COUNT (log.status), 3) AS error 
+            FROM log
+            GROUP BY date(time)
+            ORDER BY error desc) AS subq WHERE error > 1;
+"""
+
+#the cursor execute 
+
+c.execute(query3 )
+
+#the results
+
+myresults3=c.fetchall()
+
+#print the query 3
+
+print("The QUERY 3"+"\n"+query_title3 + "\n")
+
+for result in myresults3:
+        print('\t' + str(result[0]) + " - " + str(result[1]) + " % "+"\n")
+conn.close()
